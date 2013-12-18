bb1aff39ae4f1988a081c96ff9c1e72f5662de22
tkmagesh <tkmagesh77@gmail.com>
2013-12-18 12:06:01 -0800

    Collection Manipulation using Functions

--- Day-3/products.js
+++ Day-3/products.js
@@ -6,3 +6,23 @@ var products = [
 	{id:8,name:"ken",cost:12,units:6},
 	{id:5,name:"hen",cost:90,units:7}
 ]
+
+function min(list,attrName){
+	var result = list[0][attrName];
+	for(var i=1;i<list.length;i++)
+		if (result > list[i][attrName])
+			result = list[i][attrName];
+	return result;
+}
+
+function min(list,attrSelectorFn){
+	var result = attrSelectorFn(list[0]);
+	for(var i=1;i<list.length;i++)
+		if (result > attrSelectorFn(list[i]))
+			result = attrSelectorFn(list[i]);
+	return result;
+}
+
+function idSelector(p){ return p.id; }
+
+console.log(min(products,idSelector));
