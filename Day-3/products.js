3cd340e8115b983ffedf10928bb754d1c3aedda6
tkmagesh <tkmagesh77@gmail.com>
2013-12-18 11:00:44 -0800

    Products seed data

---
+++ Day-3/products.js
@@ -0,0 +1,8 @@
+var products = [
+	{id:2,name:"pen",cost:72,units:2},
+	{id:4,name:"len",cost:82,units:9},
+	{id:1,name:"den",cost:25,units:3},
+	{id:3,name:"ten",cost:76,units:4},
+	{id:8,name:"ken",cost:12,units:6},
+	{id:5,name:"hen",cost:90,units:7}
+]
